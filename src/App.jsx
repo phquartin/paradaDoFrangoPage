56f5d1bfbb649193f7ab13a403ba2f5fe17cbf67
phquartin <phquartinmatos@sempreceub.com>
2025-09-10 21:02:31 -0300

    init: Configurações iniciais aplicadas
    - Config automatica de Vite com React
    
    - Adição do plugin tailwindcss
    
    -m Página inicial de teste criada e execução bem sucedida

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return(
+        <div className={"bg-amber-300 h-screen w-screen"}>
+           <h1 className={"font-bold text-center text-orange-500 text-5xl py-8 "}>Parada do Frango</h1>
+        </div>
+    )
+}
